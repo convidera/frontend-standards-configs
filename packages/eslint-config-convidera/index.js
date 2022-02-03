20c04dc9b4aa635531176f873ec27c6bd8560f2b
deathmaz <00maz1987@gmail.com>
2022-02-03 15:53:16 +0200

    eslint: rules customization

--- packages/eslint-config-convidera/index.js
+++ packages/eslint-config-convidera/index.js
@@ -7,4 +7,70 @@ module.exports = {
     es6: true,
     node: true,
   },
+  rules: {
+    'brace-style': [
+      'error',
+      '1tbs',
+      {
+        allowSingleLine: false,
+      },
+    ],
+    curly: [
+      'error',
+      'all',
+    ],
+    'array-element-newline': [
+      'error',
+      'always',
+    ],
+
+    'array-bracket-newline': [
+      'error',
+      {
+        multiline: true,
+        minItems: 1,
+      },
+    ],
+
+    'object-property-newline': [
+      2,
+      {
+        allowAllPropertiesOnSameLine: false,
+      },
+    ],
+    'object-curly-newline': [
+      'error',
+      {
+        ObjectExpression: {
+          multiline: true,
+          minProperties: 1,
+        },
+        ObjectPattern: {
+          multiline: true,
+          minProperties: 1,
+        },
+        ImportDeclaration: {
+          multiline: true,
+          minProperties: 1,
+        },
+        ExportDeclaration: {
+          multiline: true,
+          minProperties: 1,
+        },
+      },
+    ],
+    'no-console': [
+      'warn',
+      {
+        allow: [
+          'warn',
+          'error',
+        ],
+      },
+    ],
+    'linebreak-style': [
+      2,
+      'unix',
+    ],
+  },
 };
