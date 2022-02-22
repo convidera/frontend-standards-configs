cf119fbd1124ef0eb0152a0ecc1db5b4f167bce8
deathmaz <00maz1987@gmail.com>
2022-02-22 13:59:44 +0200

    stylelint changes

--- packages/stylelint-config-vue/stylelint.config.js
+++ packages/stylelint-config-vue/stylelint.config.js
@@ -4,4 +4,26 @@ module.exports = {
     'stylelint-config-html/vue',
     '@convidera-team/stylelint-config-convidera',
   ],
+  rules: {
+    'scss/dollar-variable-empty-line-before': null,
+    'selector-pseudo-class-no-unknown': [
+      true,
+      {
+        ignorePseudoClasses: [
+          'deep',
+          'global',
+        ],
+      },
+    ],
+    'selector-pseudo-element-no-unknown': [
+      true,
+      {
+        ignorePseudoElements: [
+          'v-deep',
+          'v-global',
+          'v-slotted',
+        ],
+      },
+    ],
+  },
 };
